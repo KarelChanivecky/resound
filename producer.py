3bc6e5929ef4615fc6ec78fc2893beb52b7b511b
karelchanivecky <karl_chanivecky@hotmail.com>
2021-02-02 06:50:55 -0800

    FT: Add produce method to producer

--- producer.py
+++ producer.py
@@ -2,6 +2,14 @@
 class Producer:
     """
     Models a threaded producer.
+
+    1- User instantiates consumer
+    2- Instantiates producer by passing consumer
+    3- Invoke start_producing
+        within:
+        a) Invoke consumer.verify()
+        b) Invoke your own logic
+        c) pass produced object to produce
     """
     def __init__(self, consumer):
         self.consumer = consumer
@@ -24,6 +32,13 @@ class Producer:
         Stop producing. Ensure to set consumer to stop consuming
         """
         pass
+
+    def __produce(self, obj):
+        """
+        Pass an object to consumer.
+        :param obj: any
+        """
+        self.consumer.give(obj)
 
     def __give_to_consumer(self, obj):
         """
