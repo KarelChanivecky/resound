6aaf7b44f20727c25fb299894a9e33ed5b228bcc
karelchanivecky <karl_chanivecky@hotmail.com>
2021-02-18 00:48:40 -0800

    changed algorithm to evaluate frequency

--- proofs_of_concept/get_frequency/freqs.py
+++ proofs_of_concept/get_frequency/freqs.py
@@ -1,35 +1,50 @@
 import sounddevice as soundd
 import numpy as np
+import scipy.signal.windows as wins
 import matplotlib.pyplot as plt
 
 SAMPLE_RATE = 5000
+FFT_SIZE = 1024
+FFT_TIME_RESOLUTION = FFT_SIZE / SAMPLE_RATE
+FFT_FREQ_RESOLUTION = 1 / FFT_TIME_RESOLUTION
 SAMPLE_DURATION = 1
 CHANNELS = 1
 TARGET_Z_SCORE = 3
 
 
 def get_sample():
-    return soundd.rec((SAMPLE_RATE * SAMPLE_DURATION), SAMPLE_RATE, CHANNELS, 'int32', blocking=True)
+    return soundd.rec(FFT_SIZE, SAMPLE_RATE, CHANNELS, 'int32', blocking=True)
 
 
 def play_sample(sample):
     soundd.play(sample, SAMPLE_RATE)
 
 
-def get_fft(sample):
-    n = len(sample)
-    amplitudes = np.fft.fft(sample[:, 0])[0:int(n/2)] / n
+def normalize_32b(samples):
+    max_amp = max(samples.max(), abs(samples.min()))
+    half_range = (2 ** 32 - 1) // 2
+    return [(amp / max_amp) * half_range for amp in samples]
+
+
+def get_fft(signal):
+    n = len(signal)
+    samples = signal[:, 0]
+    normalized_samples = normalize_32b(samples)
+    # window = wins.gaussian(FFT_SIZE, std=7, sym=True)
+    window = wins.hann(FFT_SIZE, sym=False)
+    windowed_samples = normalized_samples * window
+    amplitudes = np.fft.rfft(windowed_samples)
     amplitudes[1:] = 2 * amplitudes[1:]
     amplitudes_real = np.abs(amplitudes)
-    freqs = SAMPLE_RATE * np.arange((n / 2)) / n
-    # fig, ax = plt.subplots()
-    # plt.plot(freqs, amplitudes_real, linewidth=5)
-    # ax.set_xscale('log')
-    # ax.set_yscale('log')
-    # plt.ylabel('Amplitude')
-    # plt.xlabel('Frequency [Hz]')
-    # plt.show()
-    return freqs, amplitudes
+    freqs = np.fft.rfftfreq(FFT_SIZE, 1/SAMPLE_RATE)
+    fig, ax = plt.subplots()
+    plt.plot(freqs, amplitudes_real, linewidth=5)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    plt.ylabel('Amplitude')
+    plt.xlabel('Frequency [Hz]')
+    plt.show()
+    return amplitudes_real
 
 
 def has_outlier(samples):
@@ -74,12 +89,39 @@ def get_dominant_freqs(freqs, amps):
     return evaluate_base_freq_mean(resonant_freqs)
 
 
+def get_max_bin(samples):
+    max_bin = samples[0]
+    max_bin_index = 0
+    for i in range(0, len(samples) ):
+        if max_bin < samples[i]:
+            max_bin = samples[i]
+            max_bin_index = i
+    return max_bin_index
+
+
+def gaussian_interpolation(samples):
+    """
+    Perform gaussian interpolation.
+
+    :param samples: A list of tuples: (freq, amp). Must be length == 3, with max peak in the center
+    :return: The frequency of the peak
+    """
+    max_bin = get_max_bin(samples)
+    if max_bin == 0 or max_bin == len(samples):
+        return -1
+    top = np.log(samples[max_bin + 1] / samples[max_bin - 1])
+    bottom = 2 * np.log(samples[max_bin] ** 2 /
+                        (samples[max_bin + 1] * samples[max_bin - 1]))
+    delta = top / bottom
+    return FFT_FREQ_RESOLUTION * (delta + max_bin)
+
+
 def main():
     while True:
         sample = get_sample()
-        freqs, amps = get_fft(sample)
+        amps = get_fft(sample)
         # play_sample(sample)
-        base_freq = get_dominant_freqs(freqs, amps)
+        base_freq = gaussian_interpolation(amps)
         print(f'note: {base_freq}')
 
 
