fcf5a8a614d5323a90b2077a357e7aa08ee09661
karelchanivecky <karl_chanivecky@hotmail.com>
2021-04-24 14:45:06 -0700

    RF: Reimplement recorder as ProcessInterface

---
+++ recorder_process.py
@@ -0,0 +1,39 @@
+import threading as th
+import sounddevice as soundd
+from abstracts_interfaces.process_interface import ProcessInterface
+from sound_sample import SoundSample
+
+
+class RecorderProcess(ProcessInterface):
+    """
+    Models an audio recorder. A threaded producer.
+
+    Produces a SoundSample per production cycle.
+    """
+    __CHANNELS = 1
+    __SAMPLE_TYPE = 'int32'
+
+    def __init__(self, target_frequency_max=3000, sample_duration=1):
+        """
+        Construct an instance of recorder.
+
+        Will determine sample rate from the given target_target_frequency_max as according to Nyquist's theorem.
+        :param target_frequency_max: in Hz
+        """
+        self.__sample_rate = target_frequency_max * 2
+        self.__sample_duration = sample_duration
+
+    def run(self, **kwargs):
+        """
+        Record sound and pass to consumer.
+
+        To be run threaded.
+        :param **kwargs:
+        """
+        return self.get_sample()
+
+    def get_sample(self):
+        return SoundSample(self.__sample_rate, self.__sample_duration,
+                           soundd.rec(int(self.__sample_rate * self.__sample_duration),
+                                      self.__sample_rate, RecorderProcess.__CHANNELS,
+                                      RecorderProcess.__SAMPLE_TYPE, blocking=True)[:, 0])
