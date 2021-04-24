90af87961ea30d23979a414685a20e3607928a86
karelchanivecky <karl_chanivecky@hotmail.com>
2021-04-24 15:55:48 -0700

    fixed main.py to reflect change in OO structure. Reorganized unit tests

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from concrete_threading.threaded_consumer import ThreadedConsumer
+from concrete_threading.threaded_consumer_producer import ThreadedConsumerProducer
+from concrete_threading.threaded_producer import ThreadedProducer
 from processes.frequency_extraction_process import FrequencyExtractionProcess
 from processes.mock_consumer import MockConsumerProcess
 from processes.note_identifier import NoteIdentifierProcess
@@ -5,11 +8,11 @@ from processes.recorder_process import RecorderProcess
 
 
 def main():
-    mock_consumer = MockConsumerProcess(100, 0)
-    note_identifier = NoteIdentifierProcess(mock_consumer)
-    freq_extractor = FrequencyExtractionProcess(note_identifier)
-    recorder = RecorderProcess(freq_extractor, 2500, 0.5)
-    recorder.start()
+    mock_consumer = ThreadedConsumer(10, MockConsumerProcess())
+    note_identifier = ThreadedConsumerProducer(10, mock_consumer, NoteIdentifierProcess())
+    freq_extractor = ThreadedConsumerProducer(10, note_identifier, FrequencyExtractionProcess())
+    record_producer = ThreadedProducer(freq_extractor, RecorderProcess(2500, 0.5))
+    record_producer.start()
 
 
 if __name__ == '__main__':
